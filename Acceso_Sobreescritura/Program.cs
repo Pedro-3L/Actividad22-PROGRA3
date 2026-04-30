20f4c1ae7b97787882fd3bbb791c7c463aa3970a
Pedro-3L <lando133@gmail.com>
2026-04-29 19:59:23 -0600

    ejercicio#3 funcion override

---
+++ Acceso_Sobreescritura/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("CONTROL DE ACCESOS");
+Persona p1 = new Empleado();
+p1.nombre = "Carlos";
+
+Persona p2 = new Visitante();
+p2.nombre = "Ana";
+
+Persona p3 = new Seguridad();
+p3.nombre = "Luis";
+
+Console.WriteLine();
+p1.Acceso();
+
+Console.WriteLine();
+p2.Acceso();
+
+Console.WriteLine();
+p3.Acceso();
+
+
+
+class Persona
+{
+	public string nombre;
+
+	public virtual void Acceso()
+	{
+		Console.WriteLine(nombre + " tiene acceso básico");
+	}
+}
+class Empleado : Persona
+{
+	public override void Acceso()
+	{
+		Console.WriteLine(nombre + " tiene acceso a oficinas y áreas de trabajo");
+	}
+}
+class Visitante : Persona
+{
+	public override void Acceso()
+	{
+		Console.WriteLine(nombre + " tiene acceso limitado (solo áreas públicas)");
+	}
+}
+class Seguridad : Persona
+{
+	public override void Acceso()
+	{
+		Console.WriteLine(nombre + " tiene acceso total a todas las áreas");
+	}
+}
--- Autenticación_Interfaces/Program.cs
+++ Autenticación_Interfaces/Program.cs
@@ -1,16 +1,11 @@
 ﻿Sistema s = new Sistema();
-
 s.Login("Carlos", "1234");
 s.Registrar("Carlos inició sesión");
 
 Console.WriteLine();
-
 s.Login("Miguel", "abcd");
 s.Registrar("Miguel intentó acceder");
     
-
-
-
 interface IAutenticacion
 {
 	void Login(string usuario, string contraseña);
