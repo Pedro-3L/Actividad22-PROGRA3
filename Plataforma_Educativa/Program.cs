0360fcb3563e34c798469313f1408437532e52d6
Pedro-3L <lando133@gmail.com>
2026-04-29 19:42:12 -0600

    adicion de funcion de inicio de sesion

--- Plataforma_Educativa/Program.cs
+++ Plataforma_Educativa/Program.cs
@@ -1,44 +1,48 @@
-﻿Console.WriteLine("DATOS ALUMNO");
-Alumno a = new Alumno();
+﻿Alumno a = new Alumno();
+Console.WriteLine("DATOS ALUMNO");
 a.nombre="Carlos";
 a.carne=245;
 a.edad=15;
 a.carrera="Ingenieria";
 a.dpi=114213;
 a.direccion="1-2Av zona 10";
+a.Login();
 a.MostrarInformacion();
 a.Estudiar();
 
 Console.WriteLine();
-Console.WriteLine("DATOS CATEDRATICO");
 Catedratico c = new Catedratico();
+Console.WriteLine("DATOS CATEDRATICO");
 c.nombre="Miguel";
 c.edad=35;
 c.dpi=796873;
 c.salario=550.00;
 c.direccion="13 calle 5Av zona 1";
 c.codigo_colegiado=14794;
+c.Login();
 c.MostrarInformacion();
 c.Enseñar();
 
 Console.WriteLine();
-Console.WriteLine("DATOS CORDINACION");
 Cordinacion o = new Cordinacion();
+Console.WriteLine("DATOS CORDINACION");
 o.nombre="Andrea";
 o.edad=55;
 o.dpi=1021983;
 o.direccion="12Av zona 11";
 o.cargo="Secretaria";
-
+o.Login();
 o.MostrarInformacion();
-    
 class Usuario
 {
 	public string nombre;
 	public int dpi;
 	public int edad;
 	public string direccion;
-
+	public void Login()
+	{
+		Console.WriteLine(nombre + " inició sesión");
+	}
 	public virtual void MostrarInformacion()
 	{
 		Console.WriteLine("Nombre: " + nombre);
