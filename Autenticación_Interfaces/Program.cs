25f14c2d2846c407f659d0ab7433a759d0add379
Pedro-3L <lando133@gmail.com>
2026-04-29 19:54:17 -0600

    ejercicio 2 funcion de interface

---
+++ Autenticación_Interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿Sistema s = new Sistema();
+
+s.Login("Carlos", "1234");
+s.Registrar("Carlos inició sesión");
+
+Console.WriteLine();
+
+s.Login("Miguel", "abcd");
+s.Registrar("Miguel intentó acceder");
+    
+
+
+
+interface IAutenticacion
+{
+	void Login(string usuario, string contraseña);
+}
+
+interface IAuditoria
+{
+	void Registrar(string accion);
+}
+
+class Sistema : IAutenticacion, IAuditoria
+{
+	public void Login(string usuario, string contraseña)
+	{
+		Console.WriteLine("Usuario " + usuario + " autenticado en el sistema");
+	}
+
+	public void Registrar(string accion)
+	{
+		Console.WriteLine("Auditoría: " + accion);
+	}
+}
